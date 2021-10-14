b68f5c1a5e4ffca67d8f40f16e7bfc399bb780b2
Kirill Kotov <krlktv@mail.ru>
2021-10-14 18:38:53 +0300

    feat: integrate eslint

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
 
-class Greetings extends React.Component{
-    render(){
-        const {name}=this.props;
-        return <div>Hello, {name}</div>
-    }
+// eslint-disable-next-line react/prefer-stateless-function
+class Greetings extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        Hello,
+        {name}
+      </div>
+    );
+  }
 }
 
-export default Greetings
+export default Greetings;
