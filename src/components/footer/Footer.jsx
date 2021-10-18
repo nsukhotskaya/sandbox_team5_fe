5ed74e1f091addbb325175422f6ee8dce144a732
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-10-18 15:03:47 +0300

    fix: change props in Box and import

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
-import FooterLogo from '../../assets/index';
+import assets from '../../assets';
+
+const { footerLogo } = assets;
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Footer extends React.Component {
   render() {
     return (
-      <Box sx={{
-        height: 55, width: '100%', position: 'absolute', bottom: 0, left: 0, background: '#F2F2F2', boxShadow: '0px 4.85333px 9.70667px rgba(44, 101, 77, 0.08)',
-      }}
+      <Box
+        height="55px"
+        width="100%"
+        position="absolute"
+        bottom="0"
+        left="0"
+        bgcolor="#F2F2F2"
+        boxShadow="0 5px 10px rgba(44, 101, 77, 0.08)"
       >
-        <Box sx={{
-          height: '100%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          marginRight: '10%',
-          marginLeft: '10%',
-        }}
+        <Box
+          height="100%"
+          display="flex"
+          justifyContent="space-between"
+          flexWrap="wrap"
+          alignItems="center"
+          mr="10%"
+          ml="10%"
+
         >
-          <Link
-            href="https://exadel.com/"
-            underline="none"
-          >
-            <img
-              src={FooterLogo}
-              alt="Exadel"
-            />
+          <Link href="https://exadel.com/" underline="none">
+            <img src={footerLogo} alt="Exadel" />
           </Link>
-          <Typography color="#929292" component="p">© 2021 [Team5] Exadel Internship JS&.NET&BA</Typography>
+          <Typography color="#929292" component="p">
+            © 2021 [Team5] Exadel Internship JS&.NET&BA
+          </Typography>
         </Box>
       </Box>
     );
