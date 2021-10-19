bd58d93ce9bf5b95aa2ee18e1a327557923f21e5
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-10-19 14:34:00 +0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import './App.scss';
 import React from 'react';
+import { BrowserRouter as Router, useLocation, Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router';
+
+const PagesAll = () => {
+  const location = useLocation();
+
+  return (
+    <div className="pages">
+          <Switch location={location}>
+            <Route exact path="/">
+           {/* the name of page  */}
+            </Route>
+          </Switch>
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router> 
+      <div className="App">
+        <PagesAll />
+      </div> 
+    </Router>
+ 
+  );
 }
 
 export default App;
