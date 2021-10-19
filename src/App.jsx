4b72826d20cd050d7bb23cee9686d76950f2e446
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-10-19 15:23:41 +0300

    fix: add routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter as Router, useLocation, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Content } from './components'
 
 function App() {
-  const location = useLocation();
   return (
-    <Router> 
-      <Switch location={location}>
-            <Route exact path="/">
-           {/* the name of page  */}
-            </Route>
-          </Switch>
+    <Router>
+      <Content />
     </Router>
- 
   );
 }
 
