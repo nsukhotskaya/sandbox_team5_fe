50f88a6c7622477dc8d15e4f97e439763fcfb8af
kurolek <oleksiukoleh@ukr.net>
2021-10-20 16:32:01 +0200

    remake header without inline styles/add export

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,53 @@
 import * as React from 'react';
-import { AppBar, Box, Toolbar, Button, IconButton } from '@mui/material';
-import {
-  ChatBubbleOutlineRoundedIcon,
-  NotificationsNoneRoundedIcon,
-  SettingsRoundedIcon,
-} from '@mui/icons-material';
-// import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-// import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-// import SettingsIcon from '@mui/icons-material/Settings';
 
-export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar style={{ background: '#F2F2F2' }} position="static" elevation={0}>
-        <Toolbar sx={{ justifyContent: 'flex-end' }}>
-          <IconButton
-            size="large"
-            edge="end"
-            style={{ color: 'black' }}
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <ChatBubbleOutlineRoundedIcon />
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LoginIcon from '@mui/icons-material/Login';
+
+import { Box, IconButton } from '@mui/material';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Header extends React.Component {
+  render() {
+    return (
+      <Box
+        height="60px"
+        bgcolor="#F2F2F2"
+        position="absolute"
+        width="100%"
+        left="0"
+        top="0"
+        display="flex"
+        justifyContent="flex-end"
+      >
+        <Box
+          width="10%"
+          height="100%"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          mr="20px"
+        >
+          <IconButton>
+            <ChatBubbleOutlineIcon />
           </IconButton>
-          <IconButton
-            style={{ color: 'black' }}
-            size="large"
-            edge="end"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <NotificationsNoneRoundedIcon />
+
+          <IconButton>
+            <WarningAmberIcon />
           </IconButton>
-          <IconButton
-            size="large"
-            edge="end"
-            style={{ color: 'black' }}
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <SettingsRoundedIcon />
+
+          <IconButton>
+            <LoginIcon />
           </IconButton>
-          <Button style={{ color: 'black' }}>Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    );
+  }
 }
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './footer';
 export * from './content';
+export * from './header';
