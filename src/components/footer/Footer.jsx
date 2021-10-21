bc68e2a02ff66be8ec69d05524b61e9541cc3f55
Svyatoslav Bely <svtslv17@gmail.com>
2021-10-21 06:18:59 +0300

    fix/ returned inline styles to their previosly place

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
 import { footerLogo } from '../../assets';
-import './Footer.sass';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Footer extends React.Component {
   render() {
     return (
-      <Box className="box-one">
-        <Box className="box-two">
+      <Box
+        sx={{
+          width: 'auto',
+          flexGrow: '1',
+          padding: '10px',
+          transition: 'margin 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+          backgroundColor: '#f2f2f2',
+        }}
+      >
+        <Box
+          sx={{
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            marginRight: '10%',
+            marginLeft: '10%',
+          }}
+        >
           <Link href="https://exadel.com/" underline="none">
             <img src={footerLogo} alt="Exadel" />
           </Link>
