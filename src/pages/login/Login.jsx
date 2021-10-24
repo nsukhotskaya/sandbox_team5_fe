86c95ea9b82423ad3a763308bbd46b10bfc6d7ca
Svyatoslav Bely <svtslv17@gmail.com>
2021-10-24 16:40:18 +0300

    fix: resolve problems after pulling from develop

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,43 +3,46 @@ import React from 'react';
 import { Typography, Card, Button, TextField, Stack, Box } from '@mui/material';
 import './Login.sass';
 import { Footer } from '../../components';
+import { useMediaDown } from '../../components/utils';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Box className="loginContainer">
-        <Box className="loginCardWrapper">
-          <Card variant="outlined" className="loginCard">
-            <Typography
-              m="30px"
-              variant="h5"
-              color="#1976d2"
-              textAlign="center"
-            >
-              Log in to ...
-            </Typography>
-            <Stack m="20px auto" width="250px" spacing={2} direction="column">
-              <TextField id="outlinedBasic" label="E-mail" size="small" />
-              <TextField
-                id="outlinedPasswordInput"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                size="small"
-              />
-              <Button variant="contained">Log In</Button>
-            </Stack>
-          </Card>
-        </Box>
-        <Footer />
+const Login = () => {
+  const smallScreen = useMediaDown('sm');
+  return (
+    <Box className="loginContainer">
+      <Box className="loginCardWrapper">
+        <Card
+          variant="outlined"
+          className={smallScreen ? 'loginCardMobile' : 'loginCard'}
+        >
+          <Typography
+            m={smallScreen ? '10px' : '30px'}
+            variant="h5"
+            color="#1976d2"
+            textAlign="center"
+          >
+            Log in to ...
+          </Typography>
+          <Stack
+            m={smallScreen ? '10px auto' : '20px auto'}
+            width={smallScreen ? '195px' : '250px'}
+            spacing={2}
+            direction="column"
+          >
+            <TextField id="outlinedBasic" label="E-mail" size="small" />
+            <TextField
+              id="outlinedPasswordInput"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              size="small"
+            />
+            <Button variant="contained">Log In</Button>
+          </Stack>
+        </Card>
       </Box>
-    );
-  }
-}
+      <Footer />
+    </Box>
+  );
+};
 
 export default Login;
