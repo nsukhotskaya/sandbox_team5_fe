62811b49b9e9ed77dc6b6e24a0e7f5aefef8f4f4
OstroumovAlex <92372358+OstroumovAlex@users.noreply.github.com>
2021-10-26 12:03:24 +0300

    add Routing

--- src/components/appContent/AppContent.jsx
+++ src/components/appContent/AppContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, Route, Switch } from 'react-router-dom';
-import { Login, Employee } from '../../pages';
+import { Login, Employee, ProfileCard } from '../../pages';
 
 const AppContent = () => {
   const location = useLocation();
@@ -8,6 +8,7 @@ const AppContent = () => {
     <Switch location={location}>
       <Route exact path="/" component={Login} />
       <Route exact path="/employee" component={Employee} />
+      <Route exact path="/profilecard" component={ProfileCard} />
     </Switch>
   );
 };
