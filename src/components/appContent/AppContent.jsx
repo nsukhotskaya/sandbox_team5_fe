81a1c9c4bfc9da9deaeb315dbd0002cc998694ca
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-10-26 15:06:23 +0300

    feat: add routing for candidates

--- src/components/appContent/AppContent.jsx
+++ src/components/appContent/AppContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, Route, Switch } from 'react-router-dom';
-import { Login, Employee } from '../../pages';
+import { Login, Employee, Candidates } from '../../pages';
 
 const AppContent = () => {
   const location = useLocation();
@@ -8,6 +8,7 @@ const AppContent = () => {
     <Switch location={location}>
       <Route exact path="/" component={Login} />
       <Route exact path="/employee" component={Employee} />
+      <Route exact path="/candidates" component={Candidates} />
     </Switch>
   );
 };
