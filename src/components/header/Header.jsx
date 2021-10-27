f3db4901753df135484a575599fdf78af93a2342
kurolek <oleksiukoleh@ukr.net>
2021-10-27 16:56:10 +0200

    header test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { Box, Toolbar, IconButton, AppBar } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 import MenuIcon from '@mui/icons-material/Menu';
 import { HeaderNav } from '../headerNav';
 import './Header.sass';
+import { theme } from '../../utils';
 
 class Header extends React.PureComponent {
   render() {
     const { isOpen, openSidebar } = this.props;
     return (
-      <AppBar position="fixed">
-        <Box className="header">
-          <Toolbar>
-            {!isOpen && (
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={openSidebar}
-                edge="start"
-                margin-right="2px"
-              >
-                <MenuIcon color="action" />
-              </IconButton>
-            )}
-          </Toolbar>
-          <HeaderNav />
-        </Box>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="fixed">
+          <Box className="header" backgroundColor={theme.palette.primary.light}>
+            <Toolbar>
+              {!isOpen && (
+                <IconButton
+                  aria-label="open drawer"
+                  onClick={openSidebar}
+                  edge="start"
+                  margin-right="2px"
+                >
+                  <MenuIcon color="secondary" />
+                </IconButton>
+              )}
+            </Toolbar>
+            <HeaderNav />
+          </Box>
+        </AppBar>
+      </ThemeProvider>
     );
   }
 }
