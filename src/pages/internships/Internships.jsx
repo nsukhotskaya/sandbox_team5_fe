7dbd7af7a654076c4d38e3f2b8e628ce3cb15d69
Ekaterina Beliavskaja <beliavskaja.ekaterina@gmail.com>
2021-10-28 10:51:12 +0200

    added buttons and changed styles

--- src/pages/internships/Internships.jsx
+++ src/pages/internships/Internships.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Box, Card, CardContent, CardMedia, Button, Typography } from '@mui/material';
+import { Container, Box, Card, CardContent, CardMedia, Button, Typography, IconButton, ButtonGroup } from '@mui/material';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import './Internships.sass';
 import assets from '../../assets';
 import { getFieldLabel } from '../../utils';
+
 
 
 
@@ -29,86 +32,118 @@ const mockedPrograms = [
         imageUrl: internshipGamedev,
         linkUrl: ''
     },
+    {
+        title: getFieldLabel('internships.program.title.gamedev'),
+        dateInterval: getFieldLabel('internships.program.date.gamedev'),
+        numberOfMembers: getFieldLabel('internships.program.members.gamedev'),
+        imageUrl: internshipGamedev,
+        linkUrl: ''
+    },
 ]
 
-export const Internships = () => 
-    <Box
-        position="fixed"
-        right="10px"
-        top="50px"
-        display="flex"
-        flexDirection="column"
-        background="white"
-        border="0"
-        borderRadius="5"
-        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.15)"
-        color="#0082ca"
-        padding="15px 15px"
-        maxHeight="900px"
-        marginTop="20px"
-    >
-        <Typography
-            variant="h1"
-            component="div"
-            gutterBottom
-            marginLeft="50px"
-            fontSize="35px"
-            color="#222"
-        >
-            {getFieldLabel('internships.title')}
-        </Typography>
+export const Internships = () =>
+    <Container fixed>
         <Box
             display="flex"
-            alignItems="center"
-            background="white"
-            color="#0082ca"
-            padding="30px 30px"
-            spacing="(2)"
+            flexDirection="column"
+            marginTop="80px"
         >
-            {mockedPrograms.map(product =>
-                <Card
-                    className="internshipCard"
-                    maxWidth="345px"
-                    background="#F2F2F2"
-                    boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-                    borderRadius="20px"
+            <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+            >
+                <Typography
+                    variant="h1"
+                    component="div"
+                    marginLeft="50px"
+                    fontSize="35px"
+                    color="#222"
                 >
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={product.imageUrl}
-                        alt="java script logo"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {product.title}
-                        </Typography>
-                        <Typography variant="body2" color="main-font-color">
-                            {product.dateInterval}
-                        </Typography>
-                        <Typography variant="body2" color="main-font-color">
-                            {product.numberOfMembers}
-                        </Typography>
-                    </CardContent>
-                    <Box
-                        display="flex"
-                        justifyContent="flex-end"
-                        marginRight="10px"
-                        marginBottom="10px"
+                    {getFieldLabel('internships.title')}
+                </Typography>
+                <Box
+                    width="280px"
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="space-between"
+                    marginRight="50px"
+                >
+                    <IconButton>
+                        <ManageSearchIcon
+                        fontSize="large"
+                        /> 
+                    </IconButton>
+                    <IconButton>
+                        <FilterListIcon
+                        fontSize="large"
+                        />
+                    </IconButton>
+                    <Button 
+                        variant="outlined"
+                        size="small"
+                        href="#"
+                        >
+                        {getFieldLabel('internships.btn.add-program')}
+                    </Button>
+                </Box>
+            </Box>
+            <Box
+                className="internshipsCards"
+                background="white"
+                color="#0082ca"
+                padding="30px 30px"
+            >
+                {mockedPrograms.map(product =>
+                    <Card
+                        sx={{ borderRadius: 5 }}
+                        className="internshipCard"
+                        maxWidth="345px"
+                        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
                     >
-                        <Button
-                            variant="outlined"
-                            size="small"
-                            background="white"
-                            borderRadius="5"
-                            height="40"
-                            padding="0 15px"
-                        >{getFieldLabel('internships.btn-details')}</Button>
-                    </Box>
-                </Card>
-            )}
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={product.imageUrl}
+                            alt="java script logo"
+                        />
+                        <CardContent>
+                            <Typography color="#0e518b" gutterBottom variant="h5" component="div">
+                                {product.title}
+                            </Typography>
+                            <Typography variant="body2" color="#4e4e4e">
+                                {product.dateInterval}
+                            </Typography>
+                            <Typography variant="body2" color="#4e4e4e">
+                                {product.numberOfMembers}
+                            </Typography>
+                        </CardContent>
+                        <Box
+                            display="flex"
+                            flexDirection="row"
+                            alignItems="center"
+                            marginRight="10px"
+                            marginBottom="10px"
+                            paddingLeft="50px"
+                        >
+                            <ButtonGroup variant="text" aria-label="text button group">
+                            <Button 
+                                size="small"
+                                >
+                                {getFieldLabel('internships.btn.program-info')}
+                            </Button>
+                            <Button
+                                size="small"
+                                >
+                                {getFieldLabel('internships.btn.candidates-list')}
+                            </Button>
+                            </ButtonGroup>
+                        </Box>
+                    </Card>
+                )}
+            </Box>
         </Box>
-    </Box>
+    </Container>
         
 
 
