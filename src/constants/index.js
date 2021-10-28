d7b87dd4a9c1629644f3b87d08f10e64aa37ca66
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-10-28 20:17:34 +0300

    feat: create folder constants for constant

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export { tableFields } from './tableFields';
---
+++ src/constants/tableFields.js
@@ -0,0 +1,12 @@
+export const tableFields = [
+  { field: 'applicationDate', label: 'Application Date' },
+  { field: 'location', label: 'Location' },
+  { field: 'language', label: 'Language' },
+  { field: 'internship', label: 'Internship' },
+  { field: 'englishLevel', label: 'English Level' },
+  { field: 'testTask', label: 'Test Task' },
+  { field: 'interviewer', label: 'Interviewer' },
+  { field: 'status', label: 'Status' },
+  { field: 'HR', label: 'HR' },
+  { field: 'hardSkills', label: 'Hard Skills' },
+];
