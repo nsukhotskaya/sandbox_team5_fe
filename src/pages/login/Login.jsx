68b06b8d39b672a49f44ab329ba878ec729d746b
Svyatoslav Bely <svtslv17@gmail.com>
2021-10-31 21:54:38 +0300

    feature: add Enter-key listener

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Typography, Card, Button, TextField, Stack, Box } from '@mui/material';
+import {
+  Typography,
+  Card,
+  Button,
+  TextField,
+  Stack,
+  Box,
+  FormControl,
+} from '@mui/material';
 import { userLogIn } from '../../store/actions';
 
 import './Login.sass';
@@ -21,55 +29,67 @@ const Login = (props) => {
     state[name] = value;
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = () => {
     props.userLogIn(state);
+  };
+
+  const preventDefault = (event) => {
+    event.preventDefault();
+  };
+
+  const handleForm = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
   };
 
   return (
     <Box className="loginContainer">
       <Box className="loginCardWrapper">
-        <Card
-          variant="outlined"
-          className={smallScreen ? 'loginCardMobile' : 'loginCard'}
-        >
-          <Typography
-            m={smallScreen ? '10px' : '30px'}
-            variant="h5"
-            color="#1976d2"
-            textAlign="center"
+        <FormControl onKeyPress={handleForm} onSubmit={preventDefault}>
+          <Card
+            variant="outlined"
+            className={smallScreen ? 'loginCardMobile' : 'loginCard'}
           >
-            {getFieldLabel('login.title')}
-          </Typography>
-          <Typography color="red" position="absolute" top="100px">
-            {message}
-          </Typography>
-          <Stack
-            m={smallScreen ? '10px auto' : '20px auto'}
-            width={smallScreen ? '195px' : '250px'}
-            spacing={2}
-            direction="column"
-          >
-            <TextField
-              id="outlinedBasic"
-              label={getFieldLabel('login.email')}
-              size="small"
-              name="email"
-              onInput={handleChange}
-            />
-            <TextField
-              id="outlinedPasswordInput"
-              label={getFieldLabel('login.password')}
-              name="password"
-              autoComplete="current-password"
-              size="small"
-              onInput={handleChange}
-            />
-            <Button onClick={handleSubmit} variant="contained">
-              Log In
-            </Button>
-          </Stack>
-        </Card>
+            <Typography
+              m={smallScreen ? '10px' : '30px'}
+              variant="h5"
+              color="#1976d2"
+              textAlign="center"
+            >
+              {getFieldLabel('login.title')}
+            </Typography>
+            <Typography color="red" position="absolute" top="100px">
+              {message}
+            </Typography>
+
+            <Stack
+              m={smallScreen ? '10px auto' : '20px auto'}
+              width={smallScreen ? '195px' : '250px'}
+              spacing={2}
+              direction="column"
+            >
+              <TextField
+                id="outlinedBasic"
+                label={getFieldLabel('login.email')}
+                size="small"
+                name="email"
+                onInput={handleChange}
+              />
+              <TextField
+                id="outlinedPasswordInput"
+                label={getFieldLabel('login.password')}
+                name="password"
+                autoComplete="current-password"
+                size="small"
+                onInput={handleChange}
+              />
+              <Button onClick={handleSubmit} variant="contained">
+                Log In
+              </Button>
+            </Stack>
+          </Card>
+        </FormControl>
       </Box>
       <Footer />
     </Box>
