65223e9e65c52b90550344736b692d84dad34bc5
Svyatoslav Bely <svtslv17@gmail.com>
2021-11-02 23:54:56 +0300

    refactor: change action object-wrappers

--- src/store/actions/userLogIn.js
+++ src/store/actions/userLogIn.js
@@ -2,16 +2,26 @@ import { LOGIN } from '../commands/types';
 import API from '../commands/api';
 import { history } from '../store';
 
+const loginRequest = {
+  type: LOGIN.REQUEST,
+};
+const loginSuccess = {
+  type: LOGIN.SUCCESS,
+};
+const loginFailure = {
+  type: LOGIN.FAILURE,
+};
+
 const userLogIn = (user) => async (dispatch) => {
-  dispatch({ type: LOGIN.REQUEST });
+  dispatch(loginRequest);
   try {
     const response = await API.post(`api/authenticate`, user);
     const token = response.data.accessToken;
     localStorage.setItem('accessToken', token);
-    dispatch({ type: LOGIN.SUCCESS });
+    dispatch(loginSuccess);
     history.push('/');
   } catch (error) {
-    dispatch({ type: LOGIN.FAILURE });
+    dispatch({ type: loginFailure });
   }
 };
 export default userLogIn;
--- src/store/actions/userLogOut.js
+++ src/store/actions/userLogOut.js
@@ -1,9 +1,13 @@
 import { LOGOUT } from '../commands/types';
 import { history } from '../store';
 
+const logoutSuccess = {
+  type: LOGOUT.SUCCESS,
+};
+
 const userLogOut = () => (dispatch) => {
   localStorage.removeItem('accessToken');
-  dispatch({ type: LOGOUT.SUCCESS });
+  dispatch(logoutSuccess);
   history.push('/login');
 };
 export default userLogOut;
