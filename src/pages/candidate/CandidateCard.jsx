44944731ef26be159a8ecd70575a5f92fca70767
kurolek <oleksiukoleh@ukr.net>
2021-11-02 12:45:48 +0100

    create candidateCard

---
+++ src/pages/candidate/CandidateCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+//  список данных, отзывы и оценки, связанные с кандидатом пользователи, статус, история
+
+export function CandidateCard() {
+  return (
+    <Box display="flex" height="100vh" backgroundColor="primary">
+      <Box />
+    </Box>
+  );
+}
