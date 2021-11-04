41df4b5393ba89daa31683cf1ce56358a9f8ae96
Svyatoslav Bely <svtslv17@gmail.com>
2021-11-04 11:41:41 +0300

    refactor: got rid of Main-component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Box, CssBaseline } from '@mui/material';
-import { useLocation } from 'react-router-dom';
-import { Header, Main } from '../../components';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { Header, Footer } from '../../components';
+import { Internships, Candidates, ProfileCard } from '../index';
+import './Home.sass';
 
 function Home() {
   return (
     <Box display="flex" height="100vh">
       <CssBaseline />
       <Header />
-      <Main
+      <Box
+        className="main"
         display="flex"
         flexDirection="column"
         flexGrow="1"
         flexShrink="1"
         location={useLocation()}
-      />
+      >
+        <Box id="content">
+          <Box className="card" backgroundColor="background.paper">
+            <Switch>
+              <Route exact path="/home">
+                <ProfileCard />
+              </Route>
+              <Route exact path="/home/internships">
+                <Internships />
+              </Route>
+              <Route exact path="/home/candidates">
+                <Candidates />
+              </Route>
+            </Switch>
+          </Box>
+          <Footer />
+        </Box>
+      </Box>
     </Box>
   );
 }
