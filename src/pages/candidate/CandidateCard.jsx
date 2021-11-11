5631a25a11bc8bcf8ecfcff57a2bcaeaa58da47a
kurolek <oleksiukoleh@ukr.net>
2021-11-11 17:10:58 +0100

    style fix

--- src/pages/candidate/CandidateCard.jsx
+++ src/pages/candidate/CandidateCard.jsx
@@ -5,16 +5,18 @@ import { CandidateInfo } from '../../components';
 
 export function CandidateCard() {
   return (
-    <Box display="flex" height="100%" sx={{ border: 1 }}>
+    <Box display="flex" height="100%" padding="1% 1% 0 1%">
       <Box
         borderColor="primary.main"
         width="50%"
         height="100%"
-        overflow="auto"
+        padding="1%"
         sx={{ border: 1 }}
+        overflow="auto"
       >
         <Box
           height="100%"
+          padding="1%"
           boxSizing="border-box"
           display="flex"
           flexDirection="column"
