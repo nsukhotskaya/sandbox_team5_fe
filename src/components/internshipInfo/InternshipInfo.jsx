d79dc96cd7e36b618290f04cf17ae5cbe61a7553
Ekaterina Beliavskaja <beliavskaja.ekaterina@gmail.com>
2021-11-16 19:25:29 +0100

    refactor/internshipInfo component

--- src/components/internshipInfo/InternshipInfo.jsx
+++ src/components/internshipInfo/InternshipInfo.jsx
@@ -8,15 +8,64 @@ import {
   List,
   ListItem,
   ListItemText,
+  Button,
 } from '@mui/material';
 import './InternshipInfo.sass';
 import dayjs from 'dayjs';
+import EditIcon from '@mui/icons-material/Edit';
 import { getFieldLabel } from '../../utils';
 import { useMediaDown } from '../utils';
 
-const InternshipInfo = (props) => {
-  const { internshipInfo } = props;
+const InternshipInfo = ({ internshipInfo }) => {
   const mobile = useMediaDown('md');
+
+  const internshipMainInfo = {};
+  const internshipCandidatesInfo = {};
+  const fieldsMainInfo = [
+    'locations',
+    'internshipStacks',
+    'startDate',
+    'requirements',
+    'languageType',
+  ];
+  const fieldsCandidatesInfo = [
+    'candidatesCount',
+    'declinedCandidatesCount',
+    'acceptedCandidatesCount',
+    'abandonedCandidatesCount',
+    'successfullyFinishedCandidatesCount',
+    'teamsCount',
+  ];
+
+  Object.keys(internshipInfo)
+    .filter((item) => fieldsMainInfo.includes(item))
+    .forEach((key) => {
+      if (key === 'internshipStacks') {
+        internshipMainInfo.internshipStacks =
+          internshipInfo.internshipStacks &&
+          internshipInfo.internshipStacks.map(
+            (item) => item.technologyStackType,
+          );
+      } else if (key === 'locations') {
+        internshipMainInfo.location =
+          internshipInfo.locations &&
+          internshipInfo.locations.map((item) => item.name);
+      } else if (key === 'startDate' || key === 'endDate')
+        internshipMainInfo.date = dayjs(
+          internshipInfo.startDate,
+          internshipInfo.endDate,
+        ).format('D.MM.YYYY - D.MM.YYYY');
+      else {
+        internshipMainInfo[key] = internshipInfo[key];
+      }
+    });
+
+  Object.keys(internshipInfo)
+    .filter((item) => fieldsCandidatesInfo.includes(item))
+    .forEach((key) => {
+      internshipCandidatesInfo[key] = internshipInfo[key];
+    });
+
   return (
     <Box
       className={
@@ -29,150 +78,50 @@ const InternshipInfo = (props) => {
       >
         <CardMedia component="img" image={internshipInfo.imageLink} />
         <CardContent className="cardContent">
-          <Typography variant="h4">{internshipInfo.name}</Typography>
+          <Typography variant="h4" color="primary" textAlign="center">
+            {internshipInfo.name}
+          </Typography>
           <List>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.date')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {dayjs(internshipInfo.startDate, internshipInfo.endDate).format(
-                  'D.MM.YYYY - D.MM.YYYY',
-                )}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.requirements')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.requirements}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.technology')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.internshipStacks &&
-                  internshipInfo.internshipStacks.map(
-                    (item) => item.technologyStackType,
-                  )}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.language')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.languageType}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding divider>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.location')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.locations &&
-                  internshipInfo.locations.map((item) => item.name)}
-              </Typography>
-            </ListItem>
-            <Typography variant="h6" gutterBottom>
-              {getFieldLabel('internship.page.candidates')}
-            </Typography>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.applicants')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.candidatesCount}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.rejected')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.declinedCandidatesCount}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.approve')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.acceptedCandidatesCount}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.refused')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.abandonedCandidatesCount}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.graduated')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.successfullyFinishedCandidatesCount}
-              </Typography>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    {getFieldLabel('internship.page.number.teams')}
-                  </Typography>
-                }
-              />
-              <Typography variant="body1">
-                {internshipInfo.teamsCount}
-              </Typography>
-            </ListItem>
+            {Object.keys(internshipMainInfo).map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemText
+                  primary={
+                    <Typography variant="h6">
+                      {getFieldLabel(`internship.page.${item}`)}
+                    </Typography>
+                  }
+                />
+                <Typography className="internshipInfoValue" variant="body1">
+                  {internshipMainInfo[item]}
+                </Typography>
+              </ListItem>
+            ))}
           </List>
+          <Typography variant="h6">
+            {getFieldLabel('internship.page.candidates')}
+          </Typography>
+          <List>
+            {Object.keys(internshipCandidatesInfo).map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemText
+                  primary={
+                    <Typography variant="h6">
+                      {getFieldLabel(`internship.page.${item}`)}
+                    </Typography>
+                  }
+                />
+                <Typography className="internshipInfoValue" variant="body1">
+                  {internshipCandidatesInfo[item]}
+                </Typography>
+              </ListItem>
+            ))}
+          </List>
+          <Button
+            variant="outlined"
+            endIcon={<EditIcon />}
+          >
+            Edit
+          </Button>
         </CardContent>
       </Card>
     </Box>
