f516797f54e4d9cc4ac8f908880e2df053cdcf7e
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-11-17 16:59:00 +0300

    fix: delete extra case

--- src/store/reducers/candidateList.js
+++ src/store/reducers/candidateList.js
@@ -1,4 +1,4 @@
-import { GET_CANDIDATE_LIST, UPDATE_CANDIDATE_STATUS } from '../actions';
+import { GET_CANDIDATE_LIST } from '../actions';
 
 const initialState = {
   candidates: [],
@@ -19,27 +19,7 @@ export default function candidateList(state = initialState, action) {
     }
     case GET_CANDIDATE_LIST.FAILURE:
       return { ...state, isLoading: false };
-    case UPDATE_CANDIDATE_STATUS.SUCCESS: {
-      return {
-        ...state,
-        candidates: state.candidates.map((candidate) => {
-          if (candidate.id !== action.candidateId) {
-            return candidate;
-          }
-          return {
-            ...candidate,
-            statusType: action.statusType,
-          };
-        }),
-      };
-    }
     default:
       return state;
   }
 }
-
-// export const changeCandidateStatusAC = (status, candidateId) => ({
-//   type: UPDATE_CANDIDATE_STATUS,
-//   status,
-//   candidateId,
-// });
