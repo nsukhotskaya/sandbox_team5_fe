763d949b44c3b0a07b9f57247be6d632a4333391
Svyatoslav Bely <svtslv17@gmail.com>
2021-11-23 17:06:27 +0300

    fix: change structure of feedbacks

--- src/components/candidateContent/candidateFeedbacks/CandidateFeedbacks.jsx
+++ src/components/candidateContent/candidateFeedbacks/CandidateFeedbacks.jsx
@@ -1,43 +1,108 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {Box, Typography, Rating, TextField} from '@mui/material';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box, Typography, Rating, IconButton, Collapse, TextField, Button } from '@mui/material';
 import { fetchSkillsByStackType } from '../../../store/commands';
 import './CandidateFeedbacks.sass'
-import {starLabels, roleTitle} from '../../../constants/feedback';
+import { skills } from '../../../mocks/candidateFeedbacks.json';
+
+const StarRating = ({title, grade, editMode}) => {
+  const [starsCount, setStarsCount] = React.useState(grade);
+
+  return (
+    <Box display="flex" flexDirection="row" alignItems="center" m="20px">
+      <Box display="flex" flexDirection="row" alignItems="center" width="350px" justifyContent="space-between">
+        {title && <Typography
+          variant="h6"
+          width="200px"
+          overflow="hidden"
+          textOverflow="ellipsis"
+          color={editMode ? 'primary.main' : '#c0c0c0'}
+        >
+          {title}
+        </Typography>}
+        <Rating
+          value={starsCount}
+          precision={1}
+          disabled={!editMode}
+          onChange={(event, newValue) => {setStarsCount(newValue)}}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+const CandidateFeedback = ({role}) => {
+  const [isCriteriaShown , setIsCriteriaShown] = React.useState(false);
+  const [editMode, setEditMode] = React.useState(false);
+  const handleButton = () => {
+    setIsCriteriaShown(!isCriteriaShown)
+  }
+  const handleEditMode = () => {
+    setEditMode(!editMode)
+  }
+
+  return (
+    <Box className="feedbackItem">
+      <Box className="titleSection">
+        <Typography variant="h5" width="400px" overflow="hidden" textOverflow="ellipsis" noWrap>
+          {role.name} {role.role}
+        </Typography>
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Rating value={role.generalImpression} readOnly/>
+        </Box>
+        <IconButton onClick={handleButton}>{isCriteriaShown ? <ExpandLessIcon /> : <ExpandMoreIcon />}</IconButton>
+      </Box>
+      <Collapse in={isCriteriaShown}>
+        <Box border="1px solid" borderColor="background.default" display="flex" flexDirection="column">
+          {role.skillGrades.map((skill) => 
+            <StarRating 
+            key={skill.name} 
+            title={skill.name} 
+            grade={skill.grade} 
+            editMode={editMode}/>
+          )}
+          <TextField
+          sx={{margin: '0 20px 20px 20px'}}
+          defaultValue={role.textReview}
+          multiline
+          minRows="3"
+          label="Feedback"
+          InputProps={{disabled: !editMode}}
+          />
+          <StarRating 
+            title="General Impression" 
+            grade={role.generalImpression}
+            editMode={editMode}
+          />
+          {editMode ? <Button sx={{margin: '0 20px 20px 20px'}} variant="outlined" onClick={handleEditMode}>Save</Button>
+          : 
+          <Button sx={{margin: '0 20px 20px 20px'}} variant="outlined" onClick={handleEditMode}>Edit</Button>
+          }
+        </Box>
+      </Collapse>
+    </Box>
+  )
+};
 
 const CandidateFeedbacks = ({candidateInfo}) => {
-  const [value, setValue] = React.useState(2);
-  const [hover, setHover] = React.useState(-1);
-
   const skillsList = useSelector((state) => state.skills.skills);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if(candidateInfo.stackType){dispatch(fetchSkillsByStackType(candidateInfo.stackType))};
   }, [candidateInfo.stackType]);
-  
-  if ( skillsList ) {
-    Object.values(skillsList).map((skill) => console.log(skill.name));
-  };
-  
+
+  useEffect(() => {
+    if ( skillsList ) {
+      Object.values(skillsList).map((skill) => console.log(skill.name));
+    };
+  }, [skillsList]);
 
   return(
-    <Box className="feedbackContainer">
-      {roleTitle.map((item)=>(
-        <Box className="feedbackItem" key={item}>
-          <Typography>{item}</Typography>
-          <Box display="flex" flexDirection="row">
-            <Rating
-              value={value}
-              precision={1}
-              onChange={(event, newValue) => {setValue(newValue)}}
-              onChangeActive={(event, newHover) => {setHover(newHover)}}
-            />
-              {value !== null && (<Box ml="8px">{starLabels[hover !== -1 ? hover : value]}</Box>)}
-          </Box>
-          <TextField className="feedbackText" multiline minRows="3" label="Feedback"/>
-        </Box>
-      ))}
+    <Box className="feedbackContainer" paddingRight="20px">
+      { Object.values(skills).map((role) => <CandidateFeedback key={role.role} role={role}/>)}
     </Box>
   )
 };
