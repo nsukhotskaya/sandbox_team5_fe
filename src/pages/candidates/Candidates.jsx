5018f172c6f2a3199ae887ac7c00564225dcfb62
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-11-24 20:22:38 +0300

    fix: change logics for buttons, add name for internships

--- src/pages/candidates/Candidates.jsx
+++ src/pages/candidates/Candidates.jsx
@@ -59,6 +59,8 @@ const Candidates = () => {
   const candidateSearchResult = useSelector((state) => state.searchResult.searchResult);
   const newListOfCandidates = reformatCandidates(listOfCandidates);
   const newCandidateSearchResult = reformatCandidates(candidateSearchResult);
+  const internshipName = listOfCandidates && listOfCandidates.map((item) => item.internshipName);
+
 
   useEffect(() => {
     dispatch(fetchCandidateList(requestBody));
@@ -89,13 +91,13 @@ const Candidates = () => {
 
   const getRowNodeId = (data) => data.id;
 
-  const onRowSelected = (event) => {
-    const rowSelected = event.node.isSelected();
-    const rowSelectedHR = event.node.data.statusType === 'HR';
-    if (!rowSelected) {
+  const onRowSelected = () => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data.statusType);
+    if (selectedNodes.length === 0) {
       setIsSendButtonDisabled(true);
       setIsAddToWorkButtonDisabled(true);
-    } else if (rowSelected && rowSelectedHR) {
+    } else if (selectedNodes !==0 && selectedData.includes('HR')) {
       setIsAddToWorkButtonDisabled(true);
       setIsSendButtonDisabled(false);
     } else {
@@ -107,10 +109,9 @@ const Candidates = () => {
   const addToWork = () => {
     const selectedRow = gridApi.getSelectedRows();
     const candidateId = selectedRow && selectedRow.map((item) => item.id);
-    dispatch(updateCandidateStatusById(1, candidateId));
-    dispatch(fetchCandidateList(requestBody));
+    dispatch(updateCandidateStatusById(candidateId));
     const rowNode = gridApi.getRowNode(candidateId);
-    rowNode.setSelected(false);
+    if(rowNode) rowNode.setSelected(false);
   };
 
   const candidateSearch = (event) => {
@@ -130,8 +131,8 @@ const Candidates = () => {
   return (
     <Box padding="1%" width="100%" height="100%">
       <Box className="candidatesPageHeader">
-        <Typography variant="h4" component="div" gutterBottom color="#222">
-          {getFieldLabel('candidates.internship.name')}
+        <Typography variant="h4" component="div" gutterBottom color="#929292">
+          {internshipName[0]}
         </Typography>
         <Box display="flex" alignItems="center">
           <Box marginRight="15px">
