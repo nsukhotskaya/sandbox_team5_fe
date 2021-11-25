5dc2ff59f4c996cfbc1ec2d251fb065c62d0e15b
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-11-25 14:51:03 +0300

    feat: create components

---
+++ src/components/candidatesSearch/CandidatesSearch.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+    Input,
+  } from '@mui/material';
+import { fetchCandidateSearch } from '../../store/commands';
+import { getFieldLabel } from '../../utils';
+
+
+const CandidatesSearch = ({ id }) => {
+
+    const dispatch = useDispatch();
+    const candidateSearch = (event) => {
+        const { value } = event.target;
+        dispatch(
+          fetchCandidateSearch({
+            skip: 0,
+            take: 50,
+            searchText: `${value}`,
+            sortBy: 'lastName',
+            isDesc: true,
+            internshipId: id,
+          }),
+        );
+      };
+
+    return (
+        <Input
+          placeholder={getFieldLabel('common.search')}
+          onChange={candidateSearch}
+        />
+    )
+}
+export default CandidatesSearch;
---
+++ src/components/pageSize/PageSize.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MenuItem, Box, InputLabel, FormControl, Select } from '@mui/material';
+import { valueMenuItem } from '../../constants';
+import { getFieldLabel } from '../../utils';
+
+const PageSize = ({gridApi}) => {
+     const createMenuItem = valueMenuItem.map((item) => (
+    <MenuItem value={item} key={item}>
+      {item}
+    </MenuItem>
+  )); 
+    
+  const onPageSizeChanged = (newPageSize) => {
+    const { value } = newPageSize.target;
+    gridApi.paginationSetPageSize(Number(value));
+  };
+
+  return (
+    <Box width="80px">
+    <FormControl fullWidth size="small">
+      <InputLabel>
+        {getFieldLabel('candidates.form.inputLabel')}
+      </InputLabel>
+      <Select
+        defaultValue="20"
+        label={getFieldLabel('candidates.form.inputLabel')}
+        onChange={onPageSizeChanged}
+      >
+        {createMenuItem}
+      </Select>
+    </FormControl>
+  </Box>
+  )
+
+};
+
+export default PageSize;
