6906220e31ddebd043739c19d94434a192ce03d9
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-12-03 17:26:21 +0300

    feat: use format

--- src/components/starFormatter/StarFormatter.jsx
+++ src/components/starFormatter/StarFormatter.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Rating, Box } from '@mui/material';
 
-const StarFormatter = ({ value }) => 
-   !value? (
-     <Box>{value}</Box>
-   ) : (
-     <Rating name="read-only" defaultValue={value} max={4} readOnly /> 
-   );
+const StarFormatter = ({ value }) =>
+  !value ? (
+    <Box>{value}</Box>
+  ) : (
+    <Rating name="read-only" defaultValue={value} max={4} readOnly />
+  );
 
 export default StarFormatter;
--- src/constants/reformatCandidates.js
+++ src/constants/reformatCandidates.js
@@ -7,7 +7,7 @@ export const reformatCandidates = (candidates) =>
   candidates.map((candidate) => {
     const registrDate = dayjs(`${candidate.registrationDate}`).format(
       'DD.MM.YYYY',
-    )
+    );
     const users =
       candidate.users && candidate.users.map((item) => item.userName);
     const feedbacks =
@@ -15,6 +15,14 @@ export const reformatCandidates = (candidates) =>
       candidate.feedbacks.map((item) => item.finalEvaluation);
     const [hrName, interviewerName] = users;
     const [hrFeedback, interviewerFeedback] = feedbacks;
-    
-    return { ...candidate, hr: hrName, interviewer: interviewerName, registrationDate: registrDate , hrReview: hrFeedback, interviewerReview : interviewerFeedback, fullName: `${candidate.firstName} ${candidate.lastName}`};
+
+    return {
+      ...candidate,
+      hr: hrName,
+      interviewer: interviewerName,
+      registrationDate: registrDate,
+      hrReview: hrFeedback,
+      interviewerReview: interviewerFeedback,
+      fullName: `${candidate.firstName} ${candidate.lastName}`,
+    };
   });
