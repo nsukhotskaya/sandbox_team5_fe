04da0f30584741d689544c941532052c70b32b06
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-12-03 17:25:45 +0300

    refactor: refactor code

--- src/constants/reformatCandidates.js
+++ src/constants/reformatCandidates.js
@@ -1,23 +1,20 @@
 import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
 
 export const reformatCandidates = (candidates) =>
   candidates.map((candidate) => {
-    const newObj = { ...candidate };
-    newObj.fullName = `${candidate.firstName} ${candidate.lastName}`;
-    newObj.registrationDate = dayjs(`${candidate.registrationDate}`).format(
+    const registrDate = dayjs(`${candidate.registrationDate}`).format(
       'DD.MM.YYYY',
-    );
+    )
     const users =
       candidate.users && candidate.users.map((item) => item.userName);
     const feedbacks =
       candidate.feedbacks &&
       candidate.feedbacks.map((item) => item.finalEvaluation);
     const [hrName, interviewerName] = users;
-    newObj.hr = hrName;
-    newObj.interviewer = interviewerName;
     const [hrFeedback, interviewerFeedback] = feedbacks;
-    newObj.hrReview = hrFeedback;
-    newObj.interviewerReview = interviewerFeedback;
-
-    return newObj;
+    
+    return { ...candidate, hr: hrName, interviewer: interviewerName, registrationDate: registrDate , hrReview: hrFeedback, interviewerReview : interviewerFeedback, fullName: `${candidate.firstName} ${candidate.lastName}`};
   });
