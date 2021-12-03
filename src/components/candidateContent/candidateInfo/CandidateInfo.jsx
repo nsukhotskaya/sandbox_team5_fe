3214645d52225448190c4d09caf1fd0ac837d02a
kurolek <oleksiukoleh@ukr.net>
2021-12-03 18:59:47 +0100

    fix: console warning

--- src/components/candidateContent/candidateInfo/CandidateInfo.jsx
+++ src/components/candidateContent/candidateInfo/CandidateInfo.jsx
@@ -46,7 +46,7 @@ const CandidateInfo = (props) => {
 
       <Grid container spacing={2} margin="0 0 0 2%" width="100%">
         {tableCandidateInfoFields.map((item) => (
-          <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <Grid item xs={12} sm={12} md={12} lg={6} xl={6} key = {item}>
             <Typography variant="h6">
               {getFieldLabel(`candidate.info.${item}`)}
             </Typography>
