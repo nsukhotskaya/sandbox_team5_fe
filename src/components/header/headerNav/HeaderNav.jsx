29a4420ac8def872fa44d89945e54b0a59942d75
Svyatoslav Bely <svtslv17@gmail.com>
2021-12-05 20:14:45 +0300

    feature: add profile dropdown

--- src/components/header/headerNav/HeaderNav.jsx
+++ src/components/header/headerNav/HeaderNav.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import LoginIcon from '@mui/icons-material/Login';
-import { Box, IconButton } from '@mui/material';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box, IconButton, Collapse, Avatar, Typography, Button } from '@mui/material';
 import './HeaderNav.sass';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { deleteUserToken } from '../../../store/commands';
 
 function HeaderNav(props) {
+  const userInfo = useSelector((state) => state.userInfo.userInfo);
+  const [isCriteriaShown, setIsCriteriaShown] = React.useState(false);
   const { deleteUserToken: logOut } = props;
+
+  const handleShowButton = () => {
+    setIsCriteriaShown(!isCriteriaShown);
+  };
+
   return (
-    <Box height="60px" width="60px">
+    <Box position="relative">
       <Box className="headerNav">
-        <IconButton onClick={logOut}>
-          <LoginIcon />
+        <IconButton>
+          <Avatar alt="Ivan Ivanov" />
+        </IconButton>
+        <IconButton onClick={handleShowButton}>
+          {isCriteriaShown ? <ExpandLessIcon /> : <ExpandMoreIcon />}
         </IconButton>
       </Box>
+      <Collapse in={isCriteriaShown}>
+        <Box className="collapseContainer">
+          <Typography variant="h6" color="black" textAlign="center">{userInfo.userName}</Typography>
+          <Button onClick={logOut} >
+            <Typography variant="subtitle2" color="gray">LogOut</Typography>
+            <LoginIcon color="action" />
+          </Button>
+        </Box>
+      </Collapse>
     </Box>
   );
 }
