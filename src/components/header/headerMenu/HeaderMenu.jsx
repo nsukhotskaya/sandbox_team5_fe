0cb66b99e18d63fe9d5b86f7f72a38854661fa8c
Svyatoslav Bely <svtslv17@gmail.com>
2021-12-05 20:10:46 +0300

    feature: add styles to active link

--- src/components/header/headerMenu/HeaderMenu.jsx
+++ src/components/header/headerMenu/HeaderMenu.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { getFieldLabel } from '../../../utils';
 import './HeaderMenu.sass';
 
 function HeaderMenu() {
+  const path = useLocation().pathname;
   return (
     <Box>
-      <Link to="/internships" className="hederMenuLinkInternships">
+      <Link to="/internships" className={ path==="/internships" ? "headerMenuLinkInternships activeLink" : "headerMenuLinkInternships"}>
         {getFieldLabel('header.menu.link.internships')}
       </Link>
-      <Link to="/profile" className="hederMenuLinkProfile">
+      <Link to="/profile" className={ path==="/profile" ? "headerMenuLinkProfile activeLink" : "headerMenuLinkProfile"}>
         {getFieldLabel('header.menu.link.profile')}
       </Link>
     </Box>
