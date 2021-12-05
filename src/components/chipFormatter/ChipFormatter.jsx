82ae957fd5f51a496a17a1b9ac4da9a5d03a9107
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-12-05 22:50:37 +0300

    feat: use format

--- src/components/chipFormatter/ChipFormatter.jsx
+++ src/components/chipFormatter/ChipFormatter.jsx
@@ -2,31 +2,37 @@ import React from 'react';
 import { Chip } from '@mui/material';
 
 const ChipFormatter = ({ value }) => {
+  const getChipColor = () => {
+    switch (value) {
+      case 'New':
+        return 'secondary';
+      case 'HR_Review':
+        return 'primary';
+      case 'InterviewerReview':
+        return 'primary';
+      case 'TestTask':
+        return 'primary';
+      case 'Pending':
+        return 'default';
+      case 'Accepted':
+        return 'success';
+      case 'Questionable':
+        return 'warning';
+      case 'Graduated':
+        return 'success';
+      default:
+        return 'error';
+    }
+  };
 
- const  getChipColor = () => {
-  switch (value) {
-    case 'New':
-   return'secondary';
-    case 'HR_Review':
-     return 'primary';
-    case 'InterviewerReview':
-      return 'primary';
-    case 'TestTask':
-    return 'primary';
-    case 'Pending':
-      return'default';
-    case 'Accepted':
-      return 'success';
-    case 'Questionable':
-      return 'warning';
-    case 'Graduated':
-      return 'success';
-    default:
-     return 'error';
-  }
- }
-    
-  return <Chip label={value} color={getChipColor()} variant="outlined" size="small" />;
+  return (
+    <Chip
+      label={value}
+      color={getChipColor()}
+      variant="outlined"
+      size="small"
+    />
+  );
 };
 
 export default ChipFormatter;
