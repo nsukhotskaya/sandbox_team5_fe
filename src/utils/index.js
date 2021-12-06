23d40782141de0bb71cf1e9b675a24eb5d9fb42b
kurolek <oleksiukoleh@ukr.net>
2021-12-06 10:06:29 +0100

    refactor: reusable getChipColor, remove unused props

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { default as getFieldLabel } from './getFieldLabel';
 export { default as theme } from './theme';
 export { default as tableFiller } from './tableFiller';
 export { default as InternshipData } from './internshipData';
+export { default as getChipColorByStatus } from './getChipColorByStatus';
