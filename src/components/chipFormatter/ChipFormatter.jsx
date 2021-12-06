fb48c5623fdde9a60a0169d3b339e40596f2c5ff
kurolek <oleksiukoleh@ukr.net>
2021-12-06 10:27:40 +0100

    refactor: use getChipColor in ChipFormatter

--- src/components/chipFormatter/ChipFormatter.jsx
+++ src/components/chipFormatter/ChipFormatter.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import { Chip } from '@mui/material';
+import { getChipColorByStatus } from '../../utils';
 
-const ChipFormatter = ({ value }) => {
-  const getChipColor = () => {
-    switch (value) {
-      case 'New':
-        return 'secondary';
-      case 'HR_Review':
-      case 'InterviewerReview':
-      case 'TestTask':
-        return 'primary';
-      case 'Pending':
-        return 'default';
-      case 'Accepted':
-      case 'Graduated':
-        return 'success';
-      case 'Questionable':
-        return 'warning';
-      default:
-        return 'error';
-    }
-  };
-
-  return (
+const ChipFormatter = ({ value }) => 
+(
     <Chip
       label={value}
-      color={getChipColor()}
+      color={getChipColorByStatus(value)}
       variant="outlined"
       size="small"
     />
-  );
-};
+  )
 
 export default ChipFormatter;
