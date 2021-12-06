17f3f30daa0d0c103f1e4fd61b1a6a4cfba64a74
kurolek <oleksiukoleh@ukr.net>
2021-12-06 11:14:03 +0100

    Revert "fix: run format"
    This reverts commit 453cd9c524b13ef3236de5c340feaad6c125a59a.

--- src/components/chipFormatter/ChipFormatter.jsx
+++ src/components/chipFormatter/ChipFormatter.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Chip } from '@mui/material';
 import { getChipColorByStatus } from '../../utils';
 
-const ChipFormatter = ({ value }) => (
-  <Chip
-    label={value}
-    color={getChipColorByStatus(value)}
-    variant="outlined"
-    size="small"
-  />
-);
+const ChipFormatter = ({ value }) => 
+(
+    <Chip
+      label={value}
+      color={getChipColorByStatus(value)}
+      variant="outlined"
+      size="small"
+    />
+  )
 
 export default ChipFormatter;
