5850f46ea8225903fcc8601e0d4c15052fc61199
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-12-08 20:35:13 +0300

    feat: create function for data  for exportExcel

---
+++ src/constants/reformatCsvCandidates.js
@@ -0,0 +1,21 @@
+export const reformatCsvCandidates = (candidates) =>
+candidates.map((candidate) => {
+  const {fullName,registrationDate,location,languageType,stackType,englishLevelType,hr, hrReview,
+    interviewer,
+    interviewerReview,mentor,mentorReview,statusType} = candidate
+  return {
+    "Full Name": fullName,
+    "Application Date":registrationDate,
+    "Location":location,
+    "Language":languageType,
+    "Stack":stackType,
+    "English Level":englishLevelType,
+    "HR":hr,
+    "HR Review": hrReview,
+    "Interviewer":interviewer,
+    "Interviewer Review":interviewerReview,
+    "Mentor":mentor,
+    "Mentor Review":mentorReview,
+    "Status":statusType,
+  };
+});
